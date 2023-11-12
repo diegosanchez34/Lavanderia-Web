a4b7d627bb7b4a0ff2c2118ebc8e436b509f58dd
diego <diegosanchez1234567@gmail.com>
2023-11-12 15:07:54 -0300

    footer

--- pwdelacruz/settings.py
+++ pwdelacruz/settings.py
@@ -114,12 +114,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'app/static/app/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = 'app/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "app/static/app")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
